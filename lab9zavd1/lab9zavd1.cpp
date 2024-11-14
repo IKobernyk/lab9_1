d4713e2efdd60679d248897214ef9499126e76ea
Admin <Admin@DESKTOP-C20V9DS>
2024-11-14 18:41:22 +0200

    Добавьте файлы проекта.

---
+++ lab9zavd1/lab9zavd1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include<cstdlib>
+using namespace std;
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double numbers[10];
+    int positiveCount = 0;
+
+    cout << "Введіть 10 дійсних чисел: ";
+    for (int i = 0; i < 10; i++) {
+        cin >> numbers[i];
+        if (numbers[i] > 0) {
+            positiveCount++;
+        }
+    }
+
+    cout << "Кількість додатних елементів: " << positiveCount << endl;
+
+    return 0;
+}
+
